e93f42d748fab4d816b4952051f1fde560ef185d
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2024-06-25 17:41:30 +0200

    Upgrade starting scene

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <ThePadding class="pt-4">
-    <h1 class="text-4xl text-center py-6">Word Game</h1>
+    <h1 class="text-4xl text-center pt-4 pb-8"><span class="uppercase text-xl text-gray-800">Welcome to</span><br><span
+        class="text-5xl font-[jost] font-semibold">Word Game</span></h1>
     <div class="flex w-full flex-col gap-2">
       <RouterLink to="/game" class="bg-orange-500 text-white px-4 py-3 rounded-md">Start Online Game</RouterLink>
       <RouterLink to="/checker" class="bg-white px-4 py-3 rounded-md">Open Word Checker</RouterLink>
